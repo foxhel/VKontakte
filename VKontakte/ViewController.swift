0b25da7133c1d8015d0ca14c0530bd4e23329d0d
Elena Kostenko <elena@kostenko.me>
2021-04-04 14:27:30 +0200

    Initial Commit

---
+++ VKontakte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKontakte
+//
+//  Created by Elena Kostenko on 04/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
