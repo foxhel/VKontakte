ff6edce1f11605f87f0500bcd00099a3acdd4246
Elena Kostenko <elena@kostenko.me>
2021-04-07 20:55:38 +0200

    homework2

---
+++ VKontakte/Controller/TableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewController.swift
+//  VKontakte
+//
+//  Created by Elena Kostenko on 07/04/2021.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
